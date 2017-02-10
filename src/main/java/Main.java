84c805d4f6b9d38deed1ed381f09d5ea6e2e0a87
Voytsekhovskiy Vladimir <vvo@corp.muk.ua>
2017-02-10 10:07:41 +0200

    some errors fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 
 public class Main {
@@ -10,14 +11,19 @@ public class Main {
         System.out.println(Main.class.getProtectionDomain().getCodeSource().getLocation().getClass());*/
         File[] currentFileList = (new File(".")).listFiles();
 
-
-        for(File f : currentFileList){
-            if(f.getName().contains(".xls") || f.getName().contains(".xlsx")){
+        for (File f : currentFileList) {
+            if (f.getName().contains(".xls") || f.getName().contains(".xlsx")) {
                 System.out.println(f.getName());
-                checkAvailability(f);
+                try {
+                    checkAvailability(f);
+                } catch (MalformedURLException e) {
+                    System.out.println("There is a problem with the file, may be breaks. Fix it! (" + f.getName() + ")");
+                } catch (Exception e) {
+                    System.out.println("Invalid file format: " + f.getName());
+                }
             }
         }
-
+        System.out.println("Ready. Pres ENTER to exit.");
         new BufferedReader(new InputStreamReader(System.in)).readLine();
 
     }
@@ -28,15 +34,15 @@ public class Main {
         reader.read();
         ArrayList<ItemFromExcel> itemFromExcelArrayList = reader.getItemListFromExcel();
 
-        for(int j = 0; j < itemFromExcelArrayList.size(); j++){
-            System.out.println("Link "+(j+1));
+        for (int j = 0; j < itemFromExcelArrayList.size(); j++) {
+            System.out.println("Link " + (j + 1));
             ItemFromExcel itemFromExcel = itemFromExcelArrayList.get(j);
             System.out.println(itemFromExcel.getUrl());
             InfoGetter infoGetter = new InfoGetter(itemFromExcel.getUrl());
             infoGetter.connect();
             String[] resultsArray = infoGetter.getInfo();
             ExcelWriter excelWriter = new ExcelWriter(path);
-            excelWriter.writeResults(resultsArray, j+1);
+            excelWriter.writeResults(resultsArray, j + 1);
         }
     }
 }
